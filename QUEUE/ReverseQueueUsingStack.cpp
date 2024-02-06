d9155a62039a920dcadf59eb9ce4a7a324386f53
rishikarohila12 <ruhelarishika@gmail.com>
2024-02-07 02:42:56 +0530

    initial commit

--- QUEUE/ReverseQueueUsingStack.cpp
+++ QUEUE/ReverseQueueUsingStack.cpp
@@ -1,42 +1,38 @@
-#include<iostream>
-#include<queue>
-#include<stack>
+#include <iostream>
+#include <queue>
+#include <stack>
 using namespace std;
-void display(queue<int>&q)
+void display(queue<int> &q)
 {
-    int n=q.size();
-    for(int i=0;i<n;i++)
+    int n = q.size();
+    for (int i = 0; i < n; i++)
     {
-        int x=q.front();
-        cout<<x<<" ";
+        int x = q.front();
+        cout << x << " ";
         q.pop();
         q.push(x);
-        
-
     }
 }
-void reverse(queue<int>&q)
+void reverse(queue<int> &q)
 {
-    stack<int>st;
-    int n=q.size();
-    for(int i=0;i<n;i++)
+    stack<int> st;
+    int n = q.size();
+    for (int i = 0; i < n; i++)
     {
-        int x=q.front();
+        int x = q.front();
         q.pop();
         st.push(x);
-
     }
-    for(int i=0;i<n;i++)
+    for (int i = 0; i < n; i++)
     {
-        int x=st.top();
+        int x = st.top();
         st.pop();
         q.push(x);
     }
-
 }
 int main()
 {
-    queue<int>q;
+    queue<int> q;
     q.push(10);
     q.push(20);
     q.push(30);
@@ -44,13 +40,12 @@ int main()
     q.push(50);
     q.push(60);
     q.push(70);
-   display(q);
-   cout<<endl;
+    display(q);
+    cout << endl;
     reverse(q);
     display(q);
     q.pop();
-    cout<<endl;
+    cout << endl;
     display(q);
-    cout<<endl;
+    cout << endl;
 }
-    
