18f69c6fbee9131ae2c6737ddd13f5ce494d6675
rishikarohila12 <ruhelarishika@gmail.com>
2024-02-15 19:20:11 +0530

    initial commit

--- ARRAY/InsertAtParticularPos.cpp
+++ ARRAY/InsertAtParticularPos.cpp
@@ -7,24 +7,37 @@ int main()
     cout<<"Enter n"<<endl;
     cin>>n;
     int data;
-    cout<<"Enter data"<<endl;
-    cin>>data;
+    int pos;
     cout<<"Enter Elements"<<endl;
     for(int i=0;i<n;i++)
     {
         cin>>arr[i];
     }
+    cout<<"Enter data"<<endl;
+    cin>>data;
+    cout<<"Enter position where you want to insert the data"<<endl;
+    cin>>pos;
+    if(pos<0||pos>n){
+        cout<<"Invalid Position";
+    }
+    cout<<endl;
     cout<<"Before insertion:";
     for(int i=0;i<n;i++)
     {
         cout<<arr[i]<<" ";
     }
-    arr[n]=data;
-    n++;
     cout<<endl;
-    cout<<"After Insertion: ";
+    cout<<"After Insertion : ";
+    for(int i=n;i>=pos;i--)
+    {
+        arr[i]=arr[i-1];
+    }
+    arr[pos-1]=data;
+    n++;
     for(int i=0;i<n;i++)
     {
         cout<<arr[i]<<" ";
     }
 }
+
+
